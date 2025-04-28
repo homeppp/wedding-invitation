bcec1c5458d7a3cb9281ec78c7a311aa9fff2ad9
hong <homeppp@gmail.com>
2025-04-28 22:56:45 +0900

    첫 커밋

---
+++ src/main/java/com/example/demo/WeddingInvitation2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeddingInvitation2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeddingInvitation2Application.class, args);
+	}
+
+}
